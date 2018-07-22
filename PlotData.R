a800facabc5764abebe45d877aab34ef5ee2085f
richardkappa <richardkapust@hotmail.com>
2018-07-22 09:39:33 +0000

    Some more tidy

--- PlotData.R
+++ PlotData.R
@@ -6,17 +6,34 @@ require("plyr")
 require("dplyr")
 require("igraph")
 
+setwd("h:/desktop/Learning Networks/Shapefile eg/")
+
 #Get the shapefile
 shape <- readOGR(dsn = ".", layer = "EX_Sample")
 
 #Remove vertical streets
 PC_Shape <- subset(shape, substr(shape$POSTCODE, 1, 2)==shape$PC_AREA)
 
+#Plot it
+png("postcodemap.png", width=1500, height=1000)
+plot(PC_Shape)
+dev.off()
+
+#Get postcode Centroaid
+trueCentroids = gCentroid(PC_Shape,byid=TRUE)
+
 Postcodes <- PC_Shape@data
 Postcodes$ID <- seq.int(nrow(Postcodes))
 
-#Plot it
+#Add the centroid coordinates to the postcode file
+Centroids <- as.data.frame(trueCentroids@coords)
+Postcodes <- cbind(Postcodes, Centroids)
+
+#Plot it again with the centroids
+png("postcodemap_Cent.png", width=1500, height=1000)
 plot(PC_Shape)
+points(trueCentroids)
+dev.off()
 
 #Find which postcodes border which other postcodes
 tBoundaries <- gTouches(PC_Shape, byid=TRUE, returnDense = FALSE)
@@ -60,5 +77,6 @@ names(Boundaries) <- c("Postcode1", "Postcode2")
 net <- graph_from_data_frame(d=Boundaries, vertices=Postcodes, directed=F) 
 
 #plot the network
+png("postcodemap_net.png", width=1500, height=1000)
 plot(net, vertex.label=NA, vertex.size=1)
-
+dev.off()
